d23c427cdce7c571bebe1cccff9da5a878ec3e3e
Govinda Saxena <govinda.saxena@impetus.com>
2022-04-11 00:40:12 +0530

    final commit

--- src/main/java/com/test/encrypt/EncryptDecryptApplication.java
+++ src/main/java/com/test/encrypt/EncryptDecryptApplication.java
@@ -2,12 +2,15 @@ package com.test.encrypt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EncryptDecryptApplication {
+public class EncryptDecryptApplication extends ServletInitializer 
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncryptDecryptApplication.class, args);
 	}
+
 
 }
